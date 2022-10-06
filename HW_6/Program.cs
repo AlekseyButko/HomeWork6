6fc41050f867ea699c2c143495dbfbff1e9335eb
oleksii <alexbuttko@gmail.com>
2022-10-06 16:14:31 +0300

    hw6

---
+++ HW_6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HW_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose number of sustem: 2/8/10/16");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the first value");
+                string firstvalue = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Enter the second value");
+                string secondvalue = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Chose operator:+ , - , * , /");
+                char calculationOperation = Convert.ToChar(Console.ReadLine());
+                SystemNumber.Calculate(firstvalue, secondvalue, calculationOperation, number);
+            }
+        }
+    }
+}
---
+++ HW_6/SystemNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_6
+{
+    internal class SystemNumber
+    {
+        public static void Calculate(string firstvalue, string secondvalue, char calculationOperation, int numberSystem)
+        {
+            int firstValue = Convert.ToInt32(firstvalue, numberSystem);
+            int secondValue = Convert.ToInt32(secondvalue, numberSystem);
+            int result;
+            switch (calculationOperation)
+            {
+                case '+':
+                    result = firstValue + secondValue;
+                    string resultAddition = Convert.ToString(result,numberSystem);
+                    Console.WriteLine($"{firstValue} + {secondvalue} = {resultAddition}");
+                    break;
+                case '-':
+                    result = firstValue - secondValue;
+                    string resultSubtraction = Convert.ToString(result, numberSystem);
+                    Console.WriteLine($"{firstValue} - {secondValue} = {resultSubtraction}");
+                    break;
+                case '*':
+                    result = firstValue * secondValue;
+                    string resultMultiplication = Convert.ToString(result, numberSystem);
+                    Console.WriteLine($"{firstValue}*{secondValue} = {resultMultiplication}");
+                    break;
+                case '/':
+                    result = firstValue / secondValue;
+                    string resultDivision = Convert.ToString(result, numberSystem);
+                    Console.WriteLine($"{firstValue} / {secondValue} = {resultDivision}");
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
